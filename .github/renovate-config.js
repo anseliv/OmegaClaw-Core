34a94d591e04b2e5449f4ae717bbd1e6f87ddb88
anseliv <70212400+anseliv@users.noreply.github.com>
2026-07-06 13:31:07 +0300

    Update renovate-config.js

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -6,7 +6,7 @@ module.exports = {
   platform: "github",
   forkProcessing: "enabled",
   dryRun: "full",
-  repositories: ["renovate-tests/cocoapods1", "renovate-tests/gomod1", "renovatebot/github-action"],
+  repositories: ["anseliv/OmegaClaw-Core"],
   packageRules: [
     {
       description: "lockFileMaintenance",
